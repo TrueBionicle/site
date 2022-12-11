46f0841fd42d204a24f12d2a02600d3544deef47
TrueBionicle <partem1997@gmail.com>
2022-12-11 14:17:11 +0300

    brands fix

--- js/main.js
+++ js/main.js
@@ -5,31 +5,15 @@ const readMore = document.querySelectorAll('.read-more');
 function showContent () {
     readMore.forEach(item => {
         item.addEventListener('click', (e) => {
-            if(window.matchMedia("(min-width: 768px)").matches) {
-                if(e.target.classList.contains("hide")) {
-                    e.target.previousElementSibling.style.height = "auto";
-                    e.target.textContent ="Скрыть";
-                    e.target.classList.remove('hide');
-                    e.target.classList.add('show');
-                } else {
-                    e.target.previousElementSibling.style.height = "160px";
-                    e.target.textContent ="Показать всё";
-                    e.target.classList.add('hide');
-                    e.target.classList.remove('show');
-                }
-            }  else {
-                if(e.target.classList.contains("hide")) {
-                    e.target.previousElementSibling.style.height = "auto";
-                    e.target.textContent ="Скрыть";
-                    e.target.classList.remove('hide');
-                    e.target.classList.add('show');
-                } else {
-                    e.target.previousElementSibling.style.height = "88px";
-                    e.target.textContent ="Читать далее";
-                    e.target.classList.add('hide');
-                    e.target.classList.remove('show');
-                }
-            }
+            let target = e.target;
+            target.previousElementSibling.classList.toggle("content--state--height");
+            target.classList.toggle("read-more--state-transform");
+
+            if(target.textContent == "Скрыть") {
+                target.classList.contains("read-more--text--true")? target.textContent ="Читать далее" : target.textContent = "Показать всё";
+            } else {
+                target.textContent = "Скрыть";
+            } 
         });
     });
 }
